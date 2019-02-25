fdeab5f258774c59b1a6536d40835b0f13ee39c6
Nicole Sattler <nicole.sattler@live.com>
2019-02-25 12:50:49 -0600

    Update themes to include theme and display name

--- src/components/ThemePicker/ThemePicker.js
+++ src/components/ThemePicker/ThemePicker.js
@@ -2,25 +2,35 @@ import React, { Component, Fragment } from 'react'
 import './ThemePicker.css'
 
 class ThemePicker extends Component {
-  state = {
-    themes: [
-      'monokai',
-      'blackboard',
-      'duotone-dark',
-      'duotone-light',
-      'xq-dark',
-      'material',
-      'mdn-like'
-    ]
-  }
   render() {
-    const options = this.state.themes.map((theme, i) => (
-      <option value={theme} key={i}>
-        {theme}
-      </option>
-    ))
+    const themes = [
+      { theme: 'monokai', name: 'Monokai' },
+      { theme: 'blackboard', name: 'Blackboard' },
+      { theme: 'duotone-dark', name: 'Duotone Dark' },
+      { theme: 'duotone-light', name: 'Duotone Light' },
+      { theme: 'xq-dark', name: 'XQ Dark' },
+      { theme: 'material', name: 'Material' },
+      { theme: 'mdn-like', name: 'MDN Like' },
+      { theme: 'darcula', name: 'Darcula' },
+      { theme: 'dracula', name: 'Dracula' },
+      { theme: 'icecoder', name: 'Ice Coder' }
+    ]
+
+    const options = themes
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((theme, i) => (
+        <option value={theme.theme} key={i}>
+          {theme.name}
+        </option>
+      ))
+
     return (
-      <select id="theme-picker" name="theme" onChange={this.props.changeTheme}>
+      <select
+        id="theme-picker"
+        name="theme"
+        onChange={this.props.changeTheme}
+        defaultValue="monokai"
+      >
         {options}
       </select>
     )

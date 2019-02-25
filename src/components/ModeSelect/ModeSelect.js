eae3d0800fd3a5590925930c31f9429166fab057
Nicole Sattler <nicole.sattler@live.com>
2019-02-25 12:50:20 -0600

    Refactor modes

--- src/components/ModeSelect/ModeSelect.js
+++ src/components/ModeSelect/ModeSelect.js
@@ -3,12 +3,27 @@ import React, { Component } from 'react'
 class ModeSelect extends Component {
   render() {
     const { changeMode } = this.props
+
+    const modes = [
+      { mode: 'go', name: 'Go' },
+      { mode: 'javascript', name: 'JavaScript' },
+      { mode: 'lua', name: 'Lua' },
+      { mode: 'python', name: 'Python' },
+      { mode: 'php', name: 'PHP' },
+      { mode: 'ruby', name: 'Ruby' }
+    ]
+
+    const options = modes
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map((mode, i) => (
+        <option key={i} value={mode.mode}>
+          {mode.name}
+        </option>
+      ))
+
     return (
       <select name="mode" onChange={changeMode} defaultValue="javascript">
-        <option value="go">Go</option>
-        <option value="javascript">JavaScript</option>
-        <option value="lua">Lua</option>
-        <option value="python">Python</option>
+        {options}
       </select>
     )
   }

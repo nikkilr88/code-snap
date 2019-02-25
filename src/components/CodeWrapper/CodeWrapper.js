b3a151ea319c7833fd2aaa7524a86f32a4bcd6cb
Nicole Sattler <nicole.sattler@live.com>
2019-02-25 12:49:35 -0600

    Add modes and themes

--- src/components/CodeWrapper/CodeWrapper.js
+++ src/components/CodeWrapper/CodeWrapper.js
@@ -12,6 +12,8 @@ require('codemirror/mode/javascript/javascript.js')
 require('codemirror/mode/lua/lua.js')
 require('codemirror/mode/go/go.js')
 require('codemirror/mode/python/python.js')
+require('codemirror/mode/php/php.js')
+require('codemirror/mode/ruby/ruby.js')
 
 // Themes
 require('codemirror/theme/blackboard.css')
@@ -21,6 +23,9 @@ require('codemirror/theme/mdn-like.css')
 require('codemirror/theme/xq-dark.css')
 require('codemirror/theme/duotone-dark.css')
 require('codemirror/theme/duotone-light.css')
+require('codemirror/theme/darcula.css')
+require('codemirror/theme/dracula.css')
+require('codemirror/theme/icecoder.css')
 
 import './CodeWrapper.css'
 
@@ -29,11 +34,11 @@ class CodeWrapper extends Component {
     const { mode, theme, handleOnChange, color } = this.props
 
     const options = {
-      lineNumbers: false,
       mode: mode,
+      theme: theme,
+      lineNumbers: false,
       lineWrapping: true,
-      autoCloseBrackets: true,
-      theme: theme
+      autoCloseBrackets: true
     }
 
     const bgColor = {
